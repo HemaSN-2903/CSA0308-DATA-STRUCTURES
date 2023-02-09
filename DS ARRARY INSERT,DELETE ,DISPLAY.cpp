2a4d959986ed22ebe0c2232290596e88edf73f87
HemaSN-2903 <110801019+HemaSN-2903@users.noreply.github.com>
2023-02-09 09:35:40 +0530

    Add files via upload

---
+++ DS ARRARY INSERT,DELETE ,DISPLAY.cpp
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int a[100];
+    int element,i,loc,size,j,n;
+    printf("Enter the size of an array\n");
+    scanf("%d",&size);
+    printf("Enter %d array elements\n",size);
+    for(i=0;i<size;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("List before Insertion: ");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\nEnter an element to insert\n");
+    scanf("%d",&element);
+    printf("Enter a position to insert an element %d\n",element);
+    scanf("%d",&loc);
+    loc--;
+    for(i=size-1;i>=loc;i--)
+    {
+        a[i+1]=a[i];
+    }
+    a[loc]=element;
+    printf("\nList after Insertion: ");
+    for(i=0;i<size+1;i++)
+    {
+        printf("%d ",a[i]);
+    }
+
+    printf("List before deletion\n");
+    for(i=0;i<size;i++)
+    {
+        printf("%d ",a[i]);
+    }
+    printf("\nEnter an element to delete\n");
+    scanf("%d",&n);
+    for(i=0;i<size;i++)
+    {
+        if(a[i]==n)
+        {
+            for(j=i;j<(size-1);j++)
+            {
+                a[j]=a[j+1];
+            }
+            break;
+        }
+    }
+    printf("List after deletion\n");
+    for(i=0;i<(size-1);i++)
+    {
+        printf("%d ",a[i]);
+    }
+    return 0;
+}
+
+
